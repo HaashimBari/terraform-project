f243a1c1be1d6ad28459549bfb6629cf5fc57ebb
HaashimBari <barihaashim@gmail.com>
2024-02-19 03:35:38 +0000

    removed aws authentication, to be done through github actions

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  access_key = ""
-  secret_key = ""
-}
-
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
