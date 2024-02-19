5f39e10fb0081592d1ffdf2561772fd59f40e45e
HaashimBari <barihaashim@gmail.com>
2024-02-19 04:05:43 +0000

    attached public IP to ec2 instance

--- main.tf
+++ main.tf
@@ -110,6 +110,7 @@ resource "aws_security_group" "prod-SG" {
 resource "aws_instance" "example-ec2" {
   ami           = "ami-0cf10cdf9fcd62d37"
   instance_type = "t2.micro"
+  associate_public_ip_address = "true"
   subnet_id = aws_subnet.public-subnet-1.id
   security_groups = [aws_security_group.prod-SG.id]
   key_name = "main-key"
